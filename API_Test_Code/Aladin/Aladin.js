75519e004060c273617998e96e8e01c7a8a25917
HyoHyun <doch2130@gmail.com>
2023-02-11 15:25:28 +0900

    feat: 2023-02-11 1525 aladin bestBook

--- API_Test_Code/Aladin/Aladin.js
+++ API_Test_Code/Aladin/Aladin.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
 
+// 서비스 키
 const TTBKey = '키 입력';
+// 페이지 시작
+const start = '1';
+// 결과 개수
+const maxResult = '10';
+// 책 종류 - 베스트셀러
+const bookType = 'Bestseller';
 
 axios({
   method: 'get',
-  url: `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${[TTBKey]}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`
+  url: `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${[TTBKey]}&QueryType=${bookType}&MaxResults=${maxResult}&start=${start}&SearchTarget=Book&output=js&Version=20131101`
+  // url: `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${[TTBKey]}&QueryType=Bestseller&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`
 })
 .then((response) => {
   console.log(response.data);
