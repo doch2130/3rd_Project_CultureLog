6a6b882566a19cfb392c68a9a333bade91550eee
chaeyeongLEE <dh503401@naver.com>
2023-02-20 16:12:48 +0900

    feat: highlight date

--- project/src/components/YeongCalendar.jsx
+++ project/src/components/YeongCalendar.jsx
@@ -3,18 +3,42 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './YeongCalendar.css';
 import moment from 'moment';
-//import Modal from './Modal';
 import Pop from './Pop';
-import { useDispatch, useSelector } from 'react-redux';
 import { dateData } from '../actions/date_action';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function YeongCalendar() {
+export default function YeongCalendar(props) {
   const [value, setValue] = useState(new Date());
-
   const [modalShow, setModalShow] = useState(false);
 
-  console.log(value); // 내가 선택한 날짜
+  const data = props.data;
 
+  // // 날짜 문자열을 Date 객체로 변환하는 함수
+  // const parseDate = (dateString) => {
+  //   const [year, month, day] = dateString.split('-');
+  //   return new Date(year, month - 1, day);
+  // };
+
+  // // 하이라이트 표시를 위한 배열
+  // const marks = data.map({ data.date });
+
+  // 날짜 클릭 이벤트핸들러
+  const handleDayClick = (value, event) => {
+    // const selectedDate = value.getDate();
+    // console.log(value, selectedDate);
+    setModalShow(!modalShow);
+  };
+  //Date 객체를 value 매개변수로 받아와서, getDate() 메서드를 이용하여 선택한 날짜 추출
+  //이후, 선택한 날짜를 처리하는 로직을 추가
+
+  const dispatch = useDispatch();
+  const P = useSelector((state) => state.date.date);
+  // const marks = [{ P }];
+  const marks = [moment(P).format('DD-MM-YYYY')];
+
+  console.log(value); // 내가 선택한 날짜
+  //console.log(data.);
+  //console.log(moment(P).format('DD-MM-YYYY'));
   //console.log(new Date()); // 당일 날짜
   //채영님께
   //데이터를 받아서 달력에 이벤트로 띄워질 때
@@ -25,11 +49,16 @@ export default function YeongCalendar() {
       <Calendar
         onChange={setValue}
         value={value}
-        onClickDay={(value, event) => setModalShow(!modalShow)}
+        onClickDay={handleDayClick}
+        // tileClassName={({ date }) =>
+        //   marks.includes(date) ? 'highlight' : null
+        // }
+        tileClassName={({ date, view }) => {
+          if (marks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
+            return 'highlight';
+          }
+        }}
       />
-      {/* {modalShow && <Pop show={modalShow} onHide={() => setModalShow(false)} />}
-      <div className="text-gray-500 mt-5">
-        선택한 날짜 : {moment(value).format('YYYY년 MM월 DD일')} <br /> */}
       {modalShow && (
         <Pop show={modalShow} date={value} onHide={() => setModalShow(false)} />
       )}
